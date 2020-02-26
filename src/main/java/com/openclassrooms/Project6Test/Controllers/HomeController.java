9c1d54cb9c1fa5455f668ce6f5de9a9be73dcbd7
emmardo <emmardo@posteo.net>
2020-02-26 22:50:00 +0000

    Corrected xmlns thymeleaf config error in html files

--- src/main/java/com/openclassrooms/Project6Test/Controllers/HomeController.java
+++ src/main/java/com/openclassrooms/Project6Test/Controllers/HomeController.java
@@ -1,11 +1,23 @@
 package com.openclassrooms.Project6Test.Controllers;
 
+import com.openclassrooms.Project6Test.Models.User;
+import com.openclassrooms.Project6Test.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HomeController {
+
+    @Autowired
+    UserService userService;
+
+    @ModelAttribute
+    private User setupForm() {
+        return new User(); }
 
     @GetMapping("/")
     public ModelAndView home() {
@@ -21,5 +33,12 @@ public class HomeController {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("register");
         return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user) {
+
+        userService.createUserByRole(user.getEmail(), user.getPassword(), "Regular");
+        return "redirect:/login";
     }
 }
