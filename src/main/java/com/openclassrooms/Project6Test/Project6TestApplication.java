9cdbbf23abe6676d21952cefce4311ef3cbf7251
emmardo <emmardo@posteo.net>
2020-02-27 01:15:00 +0000

    Corrected xmlns thymeleaf config error in html files

--- src/main/java/com/openclassrooms/Project6Test/Project6TestApplication.java
+++ src/main/java/com/openclassrooms/Project6Test/Project6TestApplication.java
@@ -2,6 +2,7 @@ package com.openclassrooms.Project6Test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Project6TestApplication {
